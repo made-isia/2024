5d8e407129660e624bf18cf5dda3d78ad4604b3c
Andreas Gysin <andreas@ertdfgcvb.ch>
2024-04-16 11:51:30 +0200

    Crea esempio cinematica

---
+++ esempi/09_cinematica_con_vettori/sketch.js
@@ -0,0 +1,32 @@
+
+let achille = new Vec2(0, 0)
+
+
+function setup() {
+	createCanvas(500, 500)
+	background(255)
+}
+
+function mousePressed() {
+	background(255)
+}
+
+function draw() {
+	background(255)
+
+	const tartaruga = new Vec2(mouseX, mouseY)
+
+	const spostamento = tartaruga.sottrai(achille)
+
+	achille = achille.somma(spostamento.molt(0.03))
+
+	spostamento.molt(0.5).disegna(color(255,0,0), achille)
+
+	noFill()
+	// stroke(255,0,0)
+	// circle(tartaruga.x, tartaruga.y, 10, 10)
+	stroke(0,0,255)
+	circle(achille.x, achille.y, 30, 30)
+
+
+}
