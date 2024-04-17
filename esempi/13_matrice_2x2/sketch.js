1f348bd0dc7248d3097c4adbb9b30ea06d1ab657
Andreas Gysin <andreas@ertdfgcvb.ch>
2024-04-17 10:27:19 +0200

    Crea esempio 13

---
+++ esempi/13_matrice_2x2/sketch.js
@@ -0,0 +1,49 @@
+function setup() {
+	createCanvas(500, 500)
+}
+
+function draw() {
+
+	const p1 = new Vec2(0, 100)
+	const p2 = new Vec2(0, 0)
+	const p3 = new Vec2(50, 0)
+
+	let m = new Mat2()
+	//m = m.scala(mouseX * 0.01, mouseY * 0.01)
+	//m = m.ruota(mouseX * 0.01)
+	m = m.scala(1, 1)
+
+
+	const t1 = m.moltiplicaVettore(p1)
+	const t2 = m.moltiplicaVettore(p2)
+	const t3 = m.moltiplicaVettore(p3)
+
+	background(255)
+
+	fill(0)
+	noStroke()
+	text(m.toString(), 20, 40)
+
+
+	translate(width/2, height/2)
+
+
+	noFill()
+	strokeWeight(2)
+
+	stroke(200)
+	beginShape()
+	vertex(p1.x, p1.y)
+	vertex(p2.x, p2.y)
+	vertex(p3.x, p3.y)
+	endShape()
+
+	stroke(0)
+	beginShape()
+	vertex(t1.x, t1.y)
+	vertex(t2.x, t2.y)
+	vertex(t3.x, t3.y)
+	endShape()
+
+
+}
