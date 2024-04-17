1a0b1fbe09a94c1b5abaab38a24c1686baf169df
Andreas Gysin <andreas@ertdfgcvb.ch>
2024-04-17 16:30:45 +0200

    Update sketch.js

--- esempi/18_cinematica_con_matrici/sketch.js
+++ esempi/18_cinematica_con_matrici/sketch.js
@@ -1,39 +1,87 @@
 
+let mat
+let rot = 0
+let vel = 0
+let ultimo = new Vec2(0, 0)
+
+const macchina = []
+const scia = []
 
 function setup() {
-	createCanvas(500, 500)
+	createCanvas(800, 600)
 
-	background(255)
+	const b = 40
+	const a = 20
+	macchina.push(rettangolo(-b*0.3,  a*0.5, 8, 6))
+	macchina.push(rettangolo(-b*0.3, -a*0.5, 8, 6))
+	macchina.push(rettangolo( b*0.3,  a*0.5, 8, 6))
+	macchina.push(rettangolo( b*0.3, -a*0.5, 8, 6))
+	macchina.push(rettangolo(0, 0, b, a))
+
+	mat = new Mat3()
+	mat = mat.trasla(width/2, height/2)
+
+}
 
 
-	const b = 80
-	const a = 30
+const tasti = []
 
-	const r1 = rettangolo(0, 0, b, a)
-	const r2 = rettangolo( b*0.3,  a*0.5, 16, 8)
-	const r3 = rettangolo(-b*0.3,  a*0.5, 16, 8)
-	const r4 = rettangolo(-b*0.3, -a*0.5, 16, 8)
-	const r5 = rettangolo( b*0.3, -a*0.5, 16, 8)
+function keyPressed() {
+	tasti[key] = true
+}
 
-	translate (width/2, height/2)
-
-	disegna(r2)
-	disegna(r3)
-	disegna(r4)
-	disegna(r5)
-
-	disegna(r1)
-
+function keyReleased() {
+	tasti[key] = false
 }
 
 function draw() {
+	background(255)
+
+	if      (tasti["ArrowRight"]) rot = min( 0.06, rot + 0.01)
+	else if (tasti["ArrowLeft"])  rot = max(-0.06, rot - 0.01)
+	rot *= 0.9
+
+	if (tasti["ArrowUp"]) vel = min(3, vel + 0.04)
+	else vel = max(0, vel - 0.04)
+
+
+	mat = mat.ruota(rot)
+	mat = mat.trasla(vel, 0)
+
+	const nuovo = mat.moltiplicaVettore(new Vec2(0, 0))
+	const d = dist (nuovo.x, nuovo.y, ultimo.x, ultimo.y)
+
+	if (d > 8) {
+		ultimo = nuovo
+		scia.push(mat.moltiplicaVettore(new Vec2(0, -10)))
+		scia.push(mat.moltiplicaVettore(new Vec2(0,  10)))
+		if (scia.length > 900) {
+			scia.shift()
+			scia.shift()
+		}
+	}
+
+	noFill()
+	stroke(200)
+	beginShape(QUAD_STRIP)
+	for (const p of scia) {
+		vertex(p.x, p.y)
+	}
+	endShape()
+
+	fill(255, 200, 100)
+	stroke(0)
+	for (const forma of macchina) {
+		disegna(forma, mat)
+	}
 
 }
 
-function disegna(punti) {
+function disegna(punti, mat) {
 	beginShape()
 	for(const p of punti) {
-		vertex(p.x, p.y)
+		const tp = mat.moltiplicaVettore(p)
+		vertex(tp.x, tp.y)
 	}
 	endShape(CLOSE)
 }
