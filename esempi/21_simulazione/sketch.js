a35bde36f46f03bcd8c0045fedb3c0dba0792c7d
Andreas Gysin <andreas@ertdfgcvb.ch>
2024-05-16 10:57:25 +0200

    Update sketch.js

--- esempi/21_simulazione/sketch.js
+++ esempi/21_simulazione/sketch.js
@@ -1,28 +1,32 @@
 
 const sim = new Sim()
 
+let puntoSelezionato = null
 
 function setup() {
-	createCanvas(500, 500)
+	createCanvas(800, 600)
 
-
-	sim.frizione = 0.97          // default : 0.95
+	sim.frizione = 0.97    // default : 0.95
 	sim.g = new Vec2(0, 1) // default : Vec2(0, 0)
 
-	const p1 = sim.aggiungiPunto(width/2, height/2, 4)
+	const p1 = sim.aggiungiPunto(width/2, height/2, 3)
 	p1.fisso = true
 
-	const p2 = sim.aggiungiPunto(width/2+50, height/2, 4)
+	const p2 = sim.aggiungiPunto(width/2+50, height/2, 3)
 	sim.aggiungiMolla(p1, p2)
-
 }
 
 function draw() {
 	background(255)
 
 	sim.aggiorna()
-	sim.bordi(0, 0, width, height)
 
+	if (puntoSelezionato != null) {
+		puntoSelezionato.pos.x = mouseX
+		puntoSelezionato.pos.y = mouseY
+	}
+
+	sim.bordi(0, 0, width, height)
 
 	stroke(0, 200, 255)
 	for (const m of sim.molle) {
@@ -35,4 +39,12 @@ function draw() {
 	for (const p of sim.punti) {
 		rect(p.pos.x-p.raggio, p.pos.y-p.raggio, p.raggio*2, p.raggio*2)
 	}
+}
+
+function mousePressed() {
+	puntoSelezionato = sim.puntoVicinoA(mouseX, mouseY, 20)
+}
+
+function mouseReleased() {
+	puntoSelezionato = null
 }
